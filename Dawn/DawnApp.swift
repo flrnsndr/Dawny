d06f22011e16889f6b8ac9843d67c29903d59046
Florian Schneider <mgkzv8gwqq@privaterelay.appleid.com>
2025-12-28 16:14:36 +0100

    Initial Commit

---
+++ Dawn/DawnApp.swift
@@ -0,0 +1,17 @@
+//
+//  DawnApp.swift
+//  Dawn
+//
+//  Created by Flo on 28.12.25.
+//
+
+import SwiftUI
+
+@main
+struct DawnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DawnTests/DawnTests.swift
@@ -0,0 +1,17 @@
+//
+//  DawnTests.swift
+//  DawnTests
+//
+//  Created by Flo on 28.12.25.
+//
+
+import Testing
+@testable import Dawn
+
+struct DawnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
